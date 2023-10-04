5ffb74e0dce4e3589fef3fca9924a05b016eee38
trupti kawale <truptikawale1101@gmail.com>
2023-10-03 23:39:37 -0700

    java programs

---
+++ JAVA/ArrayObjectCreation/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    void display(){
+        int array[]=new int[2];
+        array[0]=5;
+        array[1]=10;
+        array[2]=15;
+    }
+    public static void main(String[] args) {
+
+
+    }
+}
