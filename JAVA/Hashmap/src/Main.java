229ff40cb8507097ecce27a8c0a69e7b6f6d5f85
trupti kawale <truptikawale1101@gmail.com>
2023-10-03 23:52:44 -0700

    java programs

---
+++ JAVA/Hashmap/src/Main.java
@@ -0,0 +1,8 @@
+import java.util.HashMap;
+
+public class Main {
+    public static void main(String[] args) {
+        HashMap<Integer,String> data=new HashMap<Integer,String>()
+                data.put(Trupti,10)
+    }
+}
