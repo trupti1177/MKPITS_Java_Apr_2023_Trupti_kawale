397f6eaf337ba33ae8a2417d40ee17f58a59bb51
trupti kawale <truptikawale1101@gmail.com>
2023-10-03 23:59:13 -0700

    java programs

---
+++ JAVA/SteamAPI/src/Main.java
@@ -0,0 +1,8 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<String> cities= Arrays.asList("wardha","Nagpur","pune",)
+    }
+}
